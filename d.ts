2e971a00a76eae2e4548efb13e2750ffc3d726f3
rohanop <rohanopdev@gmail.com>
2022-12-04 12:11:18 +0530

    day 4

---
+++ d.ts
@@ -0,0 +1,16 @@
+const input = await Deno.readTextFile("d.txt");
+const pairs = input.trim().split(/\r?\n/);
+
+function main() {
+  let count = 0;
+  pairs.map((s) => {
+    const sections = s.split(/,|-/);
+    const [a, b, x, y] = sections.map(Number);
+    if (!(b < x || y < a)) {
+      count++;
+    }
+  });
+  console.log("Count: ", count);
+}
+
+main();
